5bc2506ebd9b8c577ea97af34c6ab58b600a75c7
Sonal Satpute <sonal.satpute@gmsil.com>
2021-01-24 17:29:28 +0530

    serialize execption as JSON

---
+++ London.Api/Filters/JsonExceptionFilter.cs
@@ -0,0 +1,42 @@
+﻿using London.Api.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace London.Api.Filters
+{
+  public class JsonExceptionFilter : IExceptionFilter
+  {
+    private readonly IHostingEnvironment _environment;
+
+    public JsonExceptionFilter(IHostingEnvironment environment)
+    {
+      _environment = environment;
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+      var error = new ApiError();
+
+      if (_environment.IsDevelopment())
+      {
+        error.Message = context.Exception.Message;
+        error.Detail = context.Exception.StackTrace;
+      }
+      else 
+      {
+        error.Message = "A servver error occurred.";
+        error.Detail = context.Exception.Message;
+      }
+
+      context.Result = new ObjectResult(error)
+      {
+        StatusCode = 500
+      };
+    }
+  }
+}
--- London.Api/Startup.cs
+++ London.Api/Startup.cs
@@ -1,3 +1,4 @@
+using London.Api.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,6 +28,7 @@ namespace London.Api
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddMvc(options => options.Filters.Add<JsonExceptionFilter>());
       services.AddRouting(options => options.LowercaseUrls = true);
       services.AddControllers();
       services.AddSwaggerGen(c =>
