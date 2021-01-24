794700bb3e01c22a7b9028cc7a1e4e0d7e6a14a2
Sonal Satpute <sonal.satpute@gmsil.com>
2021-01-24 18:58:16 +0530

    Create HoteInfo resource

---
+++ London.Api/Models/Resource.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace London.Api.Models
+{
+  public abstract class Resource
+  {
+    [JsonProperty(Order = -2)] //Order -2 to put the property on top
+    public string Href { get; set; }
+  }
+}
