6774723dd56bada65654d5b8b1bb0a4515e4b1d4
Sonal Satpute <sonal.satpute@gmsil.com>
2021-01-26 17:11:58 +0530

    return room resource from controller

--- London.Api/Controllers/RoomsController.cs
+++ London.Api/Controllers/RoomsController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using London.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace London.Api.Controllers
@@ -10,10 +10,39 @@ namespace London.Api.Controllers
   [ApiController]
   public class RoomsController : ControllerBase
   {
+    private readonly HotelApiDbContext _context;
+
+    public RoomsController(HotelApiDbContext context)
+    {
+      _context = context;
+    }
+
     [HttpGet(Name = nameof(GetRooms))]
     public IActionResult GetRooms()
     {
       throw new NotImplementedException();
     }
+
+    [HttpGet("{roomId}", Name = nameof(GetRoomById))]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<Room>> GetRoomById(Guid roomId)
+    {
+      var entity = await _context.Rooms.SingleOrDefaultAsync(entity => entity.Id == roomId);
+
+      if (entity == null)
+      {
+        return NotFound();
+      }
+
+      var resource = new Room
+      {
+        Href = Url.Link(nameof(GetRoomById), new { roomId = entity.Id }),
+        Name = entity.Name,
+        Rate = entity.Rate / 100.0m
+      };
+
+      return resource;
+    }
+
   }
 }
